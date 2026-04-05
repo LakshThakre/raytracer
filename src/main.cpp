89a826e309774bb1b74698fe8957b85c6d5d99f9
Laksh <lakshthakre07@gmail.com>
2026-04-05 18:13:05 +0530

    Add a Red Sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,21 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3 &center, double radius, const ray &r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray &r)
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1.0, 0.0, 0.0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.41, 0.55, 1.0);
@@ -27,7 +40,7 @@ int main()
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width / image_height));
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
     auto camera_center = point3(0, 0, 0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
