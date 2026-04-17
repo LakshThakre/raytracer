3a488761bb9c69664d221582b03979d342ff4ce7
lakshthakre <lakshthakre@laksh.laksh>
2026-04-17 23:51:09 +0530

    Add Antialiasing

--- src/essential.h
+++ src/essential.h
@@ -2,9 +2,11 @@
 #define ESSENTIAL_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 // C++ std Usings
 
@@ -22,7 +24,14 @@ inline double degrees_to_radians(double degrees)
 {
     return degrees * (pi / 180.0);
 }
-
+inline double random_double(){
+  static std::uniform_real_distribution<double> distributon(0.0, 1.0);
+  static std::mt19937 generator;
+  return distributon(generator);
+}
+inline double random_double(double min, double max){
+  return min + (max-min) * random_double();
+}
 // Common Headers
 
 #include "color.h"
