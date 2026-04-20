508f89a7e7d6f1fe6f0b768223193a8a40d1b654
LakshThakre <lakshthakre07@gmail.com>
2026-04-20 16:06:47 +0530

    Add metallic material

--- src/sphere.h
+++ src/sphere.h
@@ -7,7 +7,9 @@
 class sphere : public hittable
 {
 public:
-    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3 &center, double radius, shared_ptr<material> mat) : center(center), radius(std::fmax(0, radius)), mat(mat) {
+      // TODO: Initialize the material pointer `mat`
+    }
 
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override
     {
@@ -37,6 +39,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -44,5 +47,6 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 #endif
